ec7112a2ea99e140e6c29999b43a7ead7334c1bc
vicvalch <vicvalch@gmail.com>
2025-05-24 23:49:22 -0600

    fix 1

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
 document.getElementById('languageToggle').addEventListener('change', function() {
   const lang = this.checked ? 'es' : 'en';
-  console.log('Language toggled to:', lang);
+  console.log("Language set to", lang);
 });
