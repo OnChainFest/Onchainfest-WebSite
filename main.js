6300253c0bbde040a50bbf86d0f4ec9cb3cb45c1
vicvalch <vicvalch@gmail.com>
2025-05-25 20:53:48 -0600

    cambios

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 
-document.getElementById('languageToggle').addEventListener('change', function() {
-  const lang = this.checked ? 'es' : 'en';
-  console.log("Language set to", lang);
-});
+function setupCarousel(id, imagePaths) {
+  const container = document.getElementById(id);
+  imagePaths.forEach((path) => {
+    const img = document.createElement('img');
+    img.src = path;
+    img.alt = 'Carousel image';
+    img.classList.add('carousel-img');
+    container.appendChild(img);
+  });
+}
+
+setupCarousel('carousel-sports', ['img/deporte1.png', 'img/deporte2.png', 'img/deporte3.png']);
+setupCarousel('carousel-music', ['img/musica1.png', 'img/musica2.png', 'img/musica3.png']);
+setupCarousel('carousel-raffles', ['img/rifas1.png', 'img/rifas2.png', 'img/rifas3.png']);
+setupCarousel('carousel-brts', ['img/brag1.png', 'img/brag2.png', 'img/brag3.png']);
