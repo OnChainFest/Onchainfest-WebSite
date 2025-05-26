353c184936ef490498c7df9fb440a3b644d47ec8
vicvalch <vicvalch@gmail.com>
2025-05-26 16:49:57 -0600

    continuamos

--- mobile.js
+++ mobile.js
@@ -1,10 +1,1 @@
-
-function toggleMenu() {
-  const menu = document.getElementById('mobileMenu');
-  menu.style.display = (menu.style.display === 'flex') ? 'none' : 'flex';
-}
-
-function closeMenu() {
-  const menu = document.getElementById('mobileMenu');
-  menu.style.display = 'none';
-}
+// dummy content for deployment
