72626febf1c145ef00068c08cc215d45005989e4
vicvalch <vicvalch@gmail.com>
2025-05-26 15:28:46 -0600

    mas cambios

--- mobile.js
+++ mobile.js
@@ -1,8 +1,7 @@
-
 function toggleMenu() {
-  const menu = document.getElementById('mobileMenu');
-  menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
+  const menu = document.getElementById("mobileMenu");
+  menu.style.display = menu.style.display === "flex" ? "none" : "flex";
 }
 function closeMenu() {
-  document.getElementById('mobileMenu').style.display = 'none';
+  document.getElementById("mobileMenu").style.display = "none";
 }
